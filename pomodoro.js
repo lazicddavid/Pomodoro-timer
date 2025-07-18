0e832a8604bab525667f182b1106e2dc642646ef
David Lazic <lazic.ddavid@gmail.com>
2025-07-18 19:49:23 +0200

    added interval

--- pomodoro.js
+++ pomodoro.js
@@ -7,6 +7,20 @@ let interval = null;
 function updateTimer() {
   let min = Math.floor(time / 60);
   let sec = time % 60;
-  if (sec < 10) sec = "0" * sec;
+  if (sec < 10) {
+    sec = "0" * sec;
+  }
+  timerEl.textContent = `${min}:${sec}`;
 }
-timerEl.textContent = `${min}:${sec}`;
+
+startBtn.addEventListener('click', () => {
+  if (interval) return;
+  interval = setInterval(() => {
+    time --;
+    updateTimer();
+    if (time <= 0) {
+      clearInterval(interval)
+      interval = null;
+    }
+  }
+}
