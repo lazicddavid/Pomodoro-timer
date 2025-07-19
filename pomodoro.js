830d3dd3b63d9caa4a3e3438e2768aa49b4e6fa4
David Lazic <lazic.ddavid@gmail.com>
2025-07-19 18:36:33 +0200

    added currentTime

--- pomodoro.js
+++ pomodoro.js
@@ -1,26 +1,28 @@
-let currentTime = 25000;
+let currentTime = 25 * 60 * 1000;
 let timeToDeduct = 1000;
 let timerInterval = null;
-const display = document.querySelector(".timer");
 
+const display = document.querySelector(".timer"); // ✅ dodato
 const startBtn = document.querySelector(".btn--start");
 const stopBtn = document.querySelector(".btn--stop");
 const resetBtn = document.querySelector(".btn--reset");
+
 function updateDisplay() {
-let totalSecond = currentTime / 1000;
-let minutes = Math.floor(totalSecond / 60);
-let seconds = totalSecond % 60;
+  let totalSecond = currentTime / 1000;
+  let minutes = Math.floor(totalSecond / 60);
+  let seconds = totalSecond % 60;
+
   if (seconds < 10) {
     display.textContent = minutes + ":0" + seconds;
   } else {
     display.textContent = minutes + ":" + seconds;
   }
+}
 
-
-startBtn.addEventListener("click", function() {
+startBtn.addEventListener("click", function () {
   if (timerInterval !== null) return;
 
- timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     if (currentTime > 0) {
       currentTime -= timeToDeduct;
       updateDisplay();
@@ -32,14 +34,12 @@ startBtn.addEventListener("click", function() {
   }, timeToDeduct);
 });
 
-
 stopBtn.addEventListener("click", function () {
   clearInterval(timerInterval);
   timerInterval = null;
   currentTime = 0;
   updateDisplay();
 });
-/*
 
 //varijabla mi treba currentTime
 //iniciram ga sa 25000.
@@ -47,4 +47,4 @@ stopBtn.addEventListener("click", function () {
 //treba mi jos varijabla const timeToDeduct.
 //iniciram ga sa 1000, predstavlja jednu sekundu koja treba da se makne sa tajmera
 //na dugme start, pokrecem setInterval koji krece od trenutne vrednosti currentTime,tj pocinje tako sto oduzima od trenutne vrednosti currentTime-a
-//dugme stop, cisti interval i podesi ga na nulu. */
+//dugme stop, cisti interval i podesi ga na nulu.
